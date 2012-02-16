0d9b6849af0d9d0418e4c210cd7adb39826b51b3
Andy Freeland <andy@andyfreeland.net>
2012-02-15 23:53:19 -0600

    added install/uninstall scripts to make my life a little better

---
+++ uninstall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+replaceLink(){
+# Assumption is that existence of .olddotfiles means deleting the link
+# won't delete anything important.
+    if [ -L ~/$1 ]
+        then
+        if [ -d ~/.olddotfiles/$1 -o -f ~/.olddotfiles/$1 ]; then
+            rm ~/$1
+            cp -rf ~/.olddotfiles/$1 ~/
+        else
+            echo $1 not found in ~/.dotfiles, so we
+            echo will not be replacing that.
+        fi
+        else
+            echo $1 link not found in ~/
+    fi
+}
+
+if [ -d ~/.olddotfiles ]
+    then
+        replaceLink .vim
+        replaceLink .bashrc
+        replaceLink .bash_profile
+        replaceLink .gitconfig
+        replaceLink .git-completion.bash
+        replaceLink .gitglobalignore
+        replaceLink .vimrc
+
+        echo "Finished transfering backed up dot files."
+        echo "Check to make sure that the contents of"
+        echo "~/.olddotfiles matches the contents of ~/"
+        echo "and run rm -rf ~/.olddotfiles to finish uninstall"
+    else
+        echo "You don't have a folder of old dotfiles."
+        echo "No way to uninstall."
+fi
