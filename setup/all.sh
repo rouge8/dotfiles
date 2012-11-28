76c9078d654cc0a45dacdd98381ffa8c51a70464
Andy Freeland <andy@andyfreeland.net>
2012-11-28 18:11:46 -0500

    ubuntu powerline font

--- setup/all.sh
+++ setup/all.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 set -e
 
+# autoenv
 if [[ ! -d ~/.autoenv ]]; then
     git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 fi
+
+# venvburrito
 if [[ ! -d ~/.venvburrito ]]; then
     curl -s https://raw.github.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh | $SHELL
 fi
 
+# python packages
 pip install --user see requests beautifulsoup4
+
+# ubuntu mono (patched for powerline)
+mkdir -p ~/.fonts
+if [[ ! -d ~/.fonts/ubuntu-mono-powerline ]]; then
+    git clone git://github.com/scotu/ubuntu-mono-powerline.git ~/.fonts/ubuntu-mono-powerline
+fi
