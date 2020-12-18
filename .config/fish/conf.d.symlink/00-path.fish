3606c0a650ee5bdcdefb1c9bfad12f317d322d37
Andy Freeland <andy@andyfreeland.net>
2020-12-17 21:42:19 -0800

    Use fish_add_path to manage $PATH
    This is available on `master` and will be part of 3.2.0.
    
    https://github.com/fish-shell/fish-shell/pull/7028

--- .config/fish/conf.d.symlink/00-path.fish
+++ .config/fish/conf.d.symlink/00-path.fish
@@ -1,21 +1,20 @@
 if status --is-login
-    eval (brew shellenv)
+    set -gx HOMEBREW_PREFIX (brew --prefix)
+
     for py_version in 3.9 3.8 3.7
-        set PATH $PATH $HOMEBREW_PREFIX/opt/python@$py_version/bin
+        fish_add_path --append $HOMEBREW_PREFIX/opt/python@$py_version/bin
     end
 
     # Volta <https://volta.sh>
     set -x VOLTA_HOME $HOME/.volta
-    set PATH $VOLTA_HOME/bin $PATH
+    fish_add_path --prepend $VOLTA_HOME/bin
 
     # Cargo
-    set PATH $HOME/.cargo/bin $PATH
+    fish_add_path --prepend $HOME/.cargo/bin
 
     # shivs
-    set PATH $HOME/.local/bin $PATH
+    fish_add_path --prepend $HOME/.local/bin
 
     # scripts
-    set PATH $PATH $HOME/bin
-
-    set -x PATH $PATH
+    fish_add_path --append $HOME/bin
 end
