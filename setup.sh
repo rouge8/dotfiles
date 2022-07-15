db292c109aecb3678e472fc70af68b9a71499bc6
Andy Freeland <andy@andyfreeland.net>
2022-07-14 21:41:16 -0700

    [setup] Install nightly Rust
    This lets me use nightly `rustfmt` and get fun options for import
    sorting.

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ brew bundle install
 # Rust
 if [[ -f "$(brew --prefix)/bin/rust-analyzer" ]]; then
   rustup --version || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --no-modify-path
-  rustup toolchain install stable
+  rustup toolchain install stable nightly
   rustup component add rust-src rustfmt clippy
 fi
 
