a088ccd65647cedb6367e236430177a80969a729
Andy Freeland <andy@andyfreeland.net>
2023-04-19 17:10:11 -0700

    [vim] Pass `offset_encoding` to `vim.lsp.util` functions
    Neovim has been throwing warning messages about this for a while.

--- .config/nvim.symlink/lua/config.lua
+++ .config/nvim.symlink/lua/config.lua
@@ -227,6 +227,8 @@ local function goto_definition(split_cmd)
     local api = vim.api
 
     local handler = function(_, result, ctx)
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+
         if result == nil or vim.tbl_isempty(result) then
             local _ = log.info() and log.info(ctx.method, "No location found")
             return nil
@@ -237,15 +239,15 @@ local function goto_definition(split_cmd)
         end
 
         if vim.tbl_islist(result) then
-            util.jump_to_location(result[1])
+            util.jump_to_location(result[1], client.offset_encoding)
 
             if #result > 1 then
-                util.set_qflist(util.locations_to_items(result))
+                util.set_qflist(util.locations_to_items(result, client.offset_encoding))
                 api.nvim_command("copen")
                 api.nvim_command("wincmd p")
             end
         else
-            util.jump_to_location(result)
+            util.jump_to_location(result, client.offset_encoding)
         end
     end
 
